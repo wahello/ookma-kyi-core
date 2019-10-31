7b27ba3415d590cf8e18d61811ca544a4f0e3612
Ookma-Kyi <40346847+ookma-kyi@users.noreply.github.com>
2019-10-30 22:44:45 -0400

    Added default index page
    If no index page is found in the database a default placeholder index
    page is shown with instructions on how to dismiss it. The initial
    template for the default index page was created.

--- pages/views.py
+++ pages/views.py
@@ -7,5 +7,9 @@ from .models import Page
 
 def page_view(request, slug="index"):
     """View function for home page of site."""
+    if slug == "index":
+        index_available = Page.objects.filter(slug='index').count()
+        if index_available == 0:
+            return render(request, 'pages/default.html')
     page = get_object_or_404(Page, slug=slug)
     return render(request, 'pages/page.html', context={'page': page})
