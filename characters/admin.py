37d2aedab1b80a615583b242746c4c8ddb4dbdb9
Ookma-Kyi <40346847+ookma-kyi@users.noreply.github.com>
2020-01-29 18:11:37 -0500

    Added models to the administration panel
    Added the character and belt models to the administration panel.

--- characters/admin.py
+++ characters/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from characters.models import Character
 
 # Register your models here.
+@admin.register(Character)
+class CharacterAdmin(admin.ModelAdmin):
+    """Administration object for Character models.
+        Defines:
+         - fields to be displayed in list view (list_display)
+         - orders fields in detail view (fields),
+        """
+    list_display = ('name', 'wins', 'loses', 'draws', 'xp')
